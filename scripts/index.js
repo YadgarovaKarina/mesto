f4c79110bdd94a26c0746131cc1d83d98d6ef254
Karina Yadgarova <kyadgarova@gmail.com>
2022-06-05 22:15:12 +0300

    Правки по проектной работе

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
 const button = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-const popupSaveButton = document.querySelector('.popup__content-submit-button');
-const formElement = document.querySelector('.popup__content');
-const nameInput = document.querySelector('.popup__content-form-name');
-const jobInput = document.querySelector('.popup__content-form-job');
+const formElement = document.querySelector('.popup__content-form');
+const nameInput = document.querySelector('.popup__content-form_form_name');
+const jobInput = document.querySelector('.popup__content-form_form_job');
 const profileName = document.querySelector('.profile__author');
 const profileJob = document.querySelector('.profile__description');
 
-
 function openPopup() {
-    popup.classList.remove('popup_hidden');
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
-    popup.classList.add('popup_hidden');
+    popup.classList.remove('popup_opened');
 }
 
 
@@ -29,14 +27,6 @@ popupCloseButton.addEventListener('click', function () {
     closePopup()
 }
 );
-
-popup.addEventListener('click', function (e) {
-    if (e.target === e.currentTarget) {
-        closePopup()
-    }
-}
-);
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
