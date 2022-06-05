8a1b004eec518670b935f9bfd25c4111004cc0e5
Karina Yadgarova <kyadgarova@gmail.com>
2022-06-05 23:20:01 +0300

    Правки по проектной работе

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-const formElement = document.querySelector('.popup__content-form');
-const nameInput = document.querySelector('.popup__content-form_name');
-const jobInput = document.querySelector('.popup__content-form_job');
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__name-form');
+const jobInput = document.querySelector('.popup__job-form');
 const profileName = document.querySelector('.profile__author');
 const profileJob = document.querySelector('.profile__description');
 
